73e5fb01cf09184b56f425bc62718e9872ec4e3f
Nicholas Mariga <dolphnikoz@gmail.com>
2021-09-28 20:49:30 +0300

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="maindiv">
+      <div>
+        <h1>🎯🎯SlimGuy ToDo App with React😎😎 </h1>
+      </div>
+      <div id="todoArea">
+        <TextField
+          id="standard-basic"
+          label="Write your ToDo here..."
+          variant="standard"
+        />
+      </div>
     </div>
   );
 }
